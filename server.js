a8c617a49ddf3a36a0b70de25c2a817065e7b6b1
adhillon0817 <avleendhillon@gmail.com>
2022-09-05 21:53:30 -0700

    did my best to add right code based off class work for final code and push

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 
     // res.json()
 app.get('/send', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+res.sendFile(path.join(__dirname, 'db/db.json'))
   )
 
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you) and return new note to client.
@@ -24,8 +24,8 @@ res.sendFile(path.join(__dirname, 'public/sendFile.html'))
     // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-    
-
+app.use(express.static('public'));
+app.usee('/', routes);
 
 // Use the 'app' to 'listen' to specific 'PORT'
 

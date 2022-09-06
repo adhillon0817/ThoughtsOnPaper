3149da9ec0415ffc69f0ffdabede1eca228df472
adhillon0817 <avleendhillon@gmail.com>
2022-09-05 21:25:05 -0700

    delelted comments and added app.get and app.use information

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-// Require Express
+// Require Express and assign variables. 
 const express = require("express");
 
+const path = require('path');
 
-// Require the 'db.json' file and store in 'notes'
-
-
-// Use Express to intialize the 'app' server
 const app = express();
 
-// GET /notes should return the notes.html file
+// Set up port
+const PORT = process.env.PORT || 3001;
+
 
 
 // GET /api/notes should read the db.json file
 
     // res.json()
+app.get('/send', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+  )
 
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you) and return new note to client.
 
     // fs.writeFile
 
-
-// GET * should return the index.html file.
+    // Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+    
 
 
 // Use the 'app' to 'listen' to specific 'PORT'
 
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`Note App listening at http://localhost:${PORT}`)
 );

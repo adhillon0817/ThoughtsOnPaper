4e268b8934cc52861d572fad5a4c90e8ae62230e
adhillon0817 <avleendhillon@gmail.com>
2022-10-18 13:12:09 -0700

    added app.get to apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const data = require ('../db/db.json');
 const {v4: uuidv4} = require('uuid');
+
+module.exports=(app) => {
+    app.get('/app/notes', (req, res) => {
+        res.sendFile(path.join(_dirname, '../db/db.json'));
+
+    });
+
+    
+}

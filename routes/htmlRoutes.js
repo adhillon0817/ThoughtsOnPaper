be25a167666ff9d920e555f8feea9f8efaf4af4d
adhillon0817 <avleendhillon@gmail.com>
2022-11-08 22:29:58 -0800

    added pranthesis in line 6 of htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const data = require ('../db/db.json');
 const PORT = 3001;
 const app = express();
 
-app.get('/', req, res) => res.send('');
+app.get('/', (req, res) => res.send(''));
 
 app.listen(PORT, () =>
     console.log('')

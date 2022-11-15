ebbd304c911e3bcc86fac68014263b20ed590b1f
adhillon0817 <avleendhillon@gmail.com>
2022-11-14 23:41:06 -0800

    changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,15 @@ const path = require('path');
 const data = require ('../db/db.json');
 const {v4: uuidv4} = require('uuid');
 
-const app = require("express").Router();
+const app = router("express").Router();
 
 
 module.exports=(app) => {
-    app.get('/notes', (req, res) => {
+    router.get('/notes', (req, res) => {
         res.sendFile(path.join(_dirname, '../db/db.json'));
     });
 
-    app.post('/notes', (req,res) => {
+    router.post('/notes', (req,res) => {
         let db = fs.readFileSync('./db/db.json');
         
         res.json(db);
@@ -22,14 +22,13 @@ module.exports=(app) => {
                 title: req.body.title,
                 text: req.body.text
             };
-            console.log(note);
+            readAndAppend(db, './db/db.json');
+            res.send('db added')
         }
-    })
+    });
 
-    app.delete('/', (req, res) => {
-        res.send("Note Deleted")
-    })
+
 
 }
 
-app.delete('./api')
+router.delete('./api')

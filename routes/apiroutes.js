c95e9534e1c0237384eace13aae9fbd0ff115cef
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 01:15:59 -0800

    added to api routes file from server files

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,31 @@
 const router = require('express').Router();
+const { notStrictEqual } = require('assert');
+const { fstat } = require('fs');
 const path = require('path');
 
+app.get('/api/notes',(req,res) => {
+    const noteId = req.params.note_id
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/', (req,res) => {
+    console.log(req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+        notes.push(newNote);
+    }
+
+    const noteString = JSON.stringify(notes);
+
+    fs.writeFile(`./db/db.json`, noteString, (err) => {
+        err
+        ? console.error(err)
+        : console.log(
+            'New note!'
+        )
+    }
+    );
+
+})

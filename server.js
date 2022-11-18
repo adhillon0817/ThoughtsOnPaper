ab0a60ac7c0e43807606b98ad9355b08f455a15d
adhillon0817 <avleendhillon@gmail.com>
2022-11-17 23:45:08 -0800

    cleaned up code based on Unit 11 assignment 17

--- server.js
+++ server.js
@@ -1,40 +1,22 @@
 // Require Express and assign variables. 
 const express = require("express");
+const path = require('path');
+//Helpers method for generating unique ids
+const uuid = require('./helpers/uuid');
 const note = require('./db/db.json');
 const fs = require('fs')
-const path = require('path');
 
 
-// const path = require('path');
-
-const app = express();
-
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
-
-// Set up port
 const PORT = process.env.PORT || 3001;
 
 
+const app = express();
 
-
-// GET /api/notes should read the db.json file
-
-    // res.json()
-// app.get('/send', (req, res) =>
-// res.sendFile(path.join(__dirname, 'db/db.json'))
-//   )
-
-// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you) and return new note to client.
-
-    // fs.writeFile
-
-    // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+
+
 
 app.get('/api/notes', (req, res) => {
   res.status(200).json(note);

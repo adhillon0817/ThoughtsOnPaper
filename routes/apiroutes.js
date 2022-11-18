8c9676f9171c54cecf1e3b4bfad57b2a1f026b04
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 01:57:33 -0800

    added chagnes to css

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { response } = require('express');
 const path = require('path');
-const { brotliDecompressSync } = require('zlib');
 
 
 //GET /api/notes should read the db.json file (day 1?)

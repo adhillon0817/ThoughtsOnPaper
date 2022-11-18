e8e6ad7ec3658090b348495612be8a437d4cd56f
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 00:57:05 -0800

    added apiroutes

--- server.js
+++ server.js
@@ -1,54 +1,61 @@
 // Require Express and assign variables. 
 const express = require("express");
-const path = require('path');
+// const path = require('path');
 //Helpers method for generating unique ids
-const uuid = require('./helpers/uuid');
-const note = require('./db/db.json');
-const fs = require('fs')
-
+// const uuid = require('./helpers/uuid');
+// const notes = require('./db/db.json');
+// const fs = require('fs')
+const htmlRoutes = require('./routes/htmlroutes');
+const apiRoutes = require('./routes/apiroutes')
 
 const PORT = process.env.PORT || 3001;
 
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
+
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+// app.get('/notes', (req, res) =>
+// res.sendFile(path.join(_direname, 'public/notes.html'))
+// );
 
 
 
 app.get('/api/notes', (req, res) => {
-  res.status(200).json(note);
+  res.status(200).json(notes);
 });
 
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(_direname, 'public/notes.html'))
-);
 
 
-app.post('/api/note', (req, res) => {
+
+app.post('/api/notes', (req, res) => {
   console.info (`${req.method} note added!`);
   
 
   const { title, text } = req.body;
 
-  if(req.body){
+  if(title && text){
     const newNote = {
       title,
       text,
     };
 
-    note.push(newNote);
+    notes.push(newNote);
 
-const textString = JSON.stringify(note);
+  
+const textString = JSON.stringify(notes);
 
 fs.writeFile(`./db/db.json`, textString, (err) =>
-
 err 
-? console.error(err)
-: console.log(
-  `new note`
+  ? console.error(err)
+  : console.log(
+    `new note`
 )
 );
 
@@ -65,12 +72,12 @@ res.status(201).json(response);
 
 });
 
-app.get('/', (req,res) {
-  res.sendFile(path.join(_direname, 'public/index.html'))
-});
+// app.get('/', (req,res) => {
+//   res.sendFile(path.join(_direname, 'public/index.html'))
+// });
 
 // Use the 'app' to 'listen' to specific 'PORT'
 
-app.listen(PORT, () =>
+app.listen(PORT, () => {
   console.log(`Note App listening at http://localhost:${PORT}`)
-);
+});
